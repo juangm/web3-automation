5718ab9bae076e9d051d572aa622b284076e50a5
juangm <juangarciamarinblog@gmail.com>
2022-03-16 12:43:59 +0100

    Create index file

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+export { DotHandler } from './polkadot/dot';
+export { BnbBcHandler } from './binance/bnbChain';
+export { BnbBscHandler } from './binance/bnbSmartChain';
+export { UsdtHandler } from './ethereum/ERC20';
+export { EthHandler } from './ethereum/ethereum';
+export { HederaHandler } from './hedera/hedera';
